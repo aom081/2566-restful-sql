b00bb4b25d5476a65573d84731f886d7e1e02efa
nattavot014 64/45 <120540313+aom081@users.noreply.github.com>
2023-07-24 09:55:47 +0700

    Add files via upload

--- router/restaurant.router.js
+++ router/restaurant.router.js
@@ -1,111 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const restaurant =require("../models/restaurant.modle");
+const express =require("express");
+const router =express.Router();
+const Restaurant = require("../controllers/restaurant.controller");
 
-//insert restaurant to database
-router.post("/restaurants014", (req,res)=>{
-    const newRestaurant = new restaurant({
-        name: req.body.name,
-        type: req.body.type,
-        imageurl: req.body.imageurl
-    })
-
-    //Insert to DB
-    //http://localhost:5000
-    restaurant.create(newRestaurant, (err, data) =>{
-        if (err) {
-            res.status(500).send({
-                message: err.message || "ERrOr"
-            })
-        }else{
-
-        }
-    })
-})
-
-
-
-router.get("/restaurants014", (req,res)=>{
-    restaurant.getAll((err,data)=>{
-        if (err) {
-            res.status(500).send({
-                message: err.message || "ERrOr"
-            })
-        } else {
-            res.send(data);
-        }
-    })
-});
-
-//Get restaurant by id http://localhost:5000/restaurants014/2
-router.get("/restaurants014/:id", (req,res) => {
-    //Number.parseInt เปลี่น text เป็น int
-    const restaurantId =Number.parseInt(req.params.id);
-    restaurant.getById(restaurantId,(err,data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(400).send({
-                    message: "Restaurant not found with this id" + restaurantId
-                })
-            } else {
-                res.status(500).send({
-                    message: err.message || "ERrOr"
-                });
-            }
-        }else{
-            res.send(data)
-        }       
-    })
-});
-
-
-
-
-router.put("/restaurants014/:id", (req,res) => {
-    const restaurantId = Number.parseInt(req.params.id);
-    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({
-            message:"Attributes can't be empty!"
-        })
+//Create a new restaurant
+//http://localhost:5000/restaurant014
+router.post("/restaurant014", async (req,res) => {
+    try{
+    const newRestaurant = req.body;
+    const createRestaurant = await Restaurant.createRestaurant(newRestaurant);
+    res.status(201).json(createRestaurant);
+    } catch (err){
+        res.status(500).json({error:"Failed to create Restaurant"});
     }
-    restaurant.updateById(restaurantId, new restaurant(req.body), (err,data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(400).send({
-                    message: "Restaurant not found with this id" + restaurantId
-                })
-            } else {
-                res.status(500).send({
-                    message: err.message || "ERrOr"
-                });
-            }
-        } else {
-            res.send(data)
-        }
-    })
 })
 
-
-
-router.delete("/restaurants014/:id", (req,res) => {
-    const restaurantId = Number.parseInt(req.params.id);
-    restaurant.deleteById(restaurantId, (err,data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(400).send({
-                    message: "Restaurant not found with this id" + restaurantId
-                })
-            } else {
-                res.status(500).send({
-                    message: err.message || "ERrOr"
-                });
-            }
-        } else {
-            res.send({
-                message: "Restaurant id:" +restaurantId +"is deleted"
-            })
-        } 
-    })
-})
 
 module.exports =router;
